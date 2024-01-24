09d6515c026686e278c7d81c737a52e53ffce883
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-01-24 13:27:36 -0300

    Caso de Uso e Design Patterns - Caso de uso de registro

---
+++ src/use-cases/register.ts
@@ -0,0 +1,34 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { hash } from "bcryptjs";
+import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (userWithSameEmail) {
+    throw new Error("User with this email already exists");
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  });
+}
